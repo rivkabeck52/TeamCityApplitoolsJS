49bc24009da52ee6be7b23ddd615514f529282ae
rivkabeck52 <rivka.beck@applitools.com>
2020-02-20 15:02:33 +0200

    first commit

---
+++ test/demo.test.js
@@ -0,0 +1,50 @@
+
+'use strict';
+
+require('chromedriver');
+const { Builder, By } = require('selenium-webdriver');
+const { Eyes, ClassicRunner, Target, RectangleSize } = require('@applitools/eyes-selenium');
+
+describe('DemoApp - ClassicRunner', function () {
+  let runner, eyes, driver;
+
+  beforeEach(async () => {
+    // Initialize the Runner for your test.
+    runner = new ClassicRunner();
+
+    // Initialize the eyes SDK (IMPORTANT: make sure your API key is set in the APPLITOOLS_API_KEY env variable).
+    eyes = new Eyes(runner);
+
+    // Use Chrome browser
+    driver = await new Builder()
+      .forBrowser('chrome')
+      // .setChromeOptions(new ChromeOptions().headless())
+      .build();
+  });
+
+  it('TeamCity Test', async () => {
+    // Start the test by setting AUT's name, test name and viewport size (width X height)
+    await eyes.open(driver, 'TeamCity integration App', 'TeamCity Test', new RectangleSize(800, 600));
+
+    // Navigate the browser to the "ACME" demo app.
+    await driver.get("https://applitools.com/helloworld?diff1");
+
+    // Visual checkpoint #2 - Check the app page.
+    await eyes.check("App Window", Target.window().fully());
+
+    // End the test.
+    await eyes.closeAsync();
+  });
+
+  afterEach(async () => {
+    // Close the browser.
+    await driver.quit();
+
+    // If the test was aborted before eyes.close was called, ends the test as aborted.
+    await eyes.abortIfNotClosed();
+
+    // Wait and collect all test results
+    const allTestResults = await runner.getAllTestResults();
+    console.log(allTestResults);
+  });
+});
